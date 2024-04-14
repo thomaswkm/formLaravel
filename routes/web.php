68cf53e15038b55608f7468a06bf242cc7d20385
t.kinderman01 <t.kinderman01@ufromail.cl>
2024-04-14 18:31:44 -0400

    login

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController; 
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/formulario', [PostController::class, 'mostrarFormulario']);
+    Route::post('/procesar_formulario', [PostController::class, 'procesarFormulario']);
+});
 
 Route::get('/', HomeController::class);
 
-Route::get('formulario', [PostController::class, 'mostrarFormulario']);
-Route::post('procesar_formulario', [PostController::class, 'procesarFormulario']);
-
-
+require __DIR__.'/auth.php';
